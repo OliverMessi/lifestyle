4f18f0887c7d0d36dc570cb3c49d2635d10c15ab
OliverMessi <562376662@qq.com>
2020-06-29 23:03:41 +0800

    创建登录页面

--- src/network/login.js
+++ src/network/login.js
@@ -3,6 +3,7 @@ import {request} from "./request";
 export function login(userName,passWord) {
   return request({
     url:"/login",
+    method:'post',
     params:{
       userName,
       passWord
